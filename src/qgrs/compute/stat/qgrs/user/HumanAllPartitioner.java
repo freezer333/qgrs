ee1cb5c27ad810a03ba12c09149e0bc8227dc031
freezer333 <scott.frees@gmail.com>
2013-02-27 17:02:44 -0500

    Population input files

--- src/qgrs/compute/stat/qgrs/user/HumanAllPartitioner.java
+++ src/qgrs/compute/stat/qgrs/user/HumanAllPartitioner.java
@@ -30,8 +30,8 @@ public class HumanAllPartitioner extends GenePartitioner {
 	@Override
 	public HashSet<GenePartition> partition(Connection c) {
 		HashSet<GenePartition> partitions = new HashSet<GenePartition>();
-		System.out.println("Warning - human partitioner restricted to 1000 results for testing");
-		String q = "SELECT accessionNumber FROM GENE WHERE SPECIES = 'Homo sapiens' LIMIT 1000";
+		//System.out.println("Warning - human partitioner restricted to 1000 results for testing");
+		String q = "SELECT accessionNumber FROM GENE WHERE SPECIES = 'Homo sapiens'";
 		GenePartition p = new GenePartition(runner, "Human Genes");
 		try {
 			Statement stmt = c.createStatement();
