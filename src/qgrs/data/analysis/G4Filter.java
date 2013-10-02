a7b378368b6039d13f9ebda27ee0a156ac318b4d
freezer333 <scott.frees@gmail.com>
2013-10-02 16:16:00 -0400

    Fitlers

--- src/qgrs/data/analysis/G4Filter.java
+++ src/qgrs/data/analysis/G4Filter.java
@@ -6,10 +6,13 @@ import qgrs.data.mongo.primitives.jongo.MRNA;
 
 public class G4Filter {
 
-	final double min_h = 95;
+	final double min_h = 90;
 	
 	public boolean acceptable(MRNA mrna, G4 g4) {
-		if ( g4.isIn3Prime() ) {
+		if ( g4.isIn3Prime() && 
+				((g4.getNumTetrads() < 3 && g4.getScore() > 13) 
+						|| 
+				  g4.getNumTetrads() >= 3 && g4.getScore() > 17 )) {
 			for ( G4H h : g4.getConservedG4s() ) {
 				if ( h.getOverallAbsoluteScore() >= min_h) {
 					return true;
