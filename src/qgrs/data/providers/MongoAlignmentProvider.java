6c06b02198029b85d932c35d69abb41e2f7d6bcc
freezer333 <scott.frees@gmail.com>
2013-10-22 12:00:07 -0400

    Cleaning up error handling when mongo is not available.

--- src/qgrs/data/providers/MongoAlignmentProvider.java
+++ src/qgrs/data/providers/MongoAlignmentProvider.java
@@ -13,21 +13,34 @@ import com.mongodb.MongoClient;
 
 public class MongoAlignmentProvider extends AlignmentProvider {
 
-	private final MongoCollection alignments;
+	private MongoCollection alignments;
 	
 	public MongoAlignmentProvider () throws UnknownHostException {
-		DB db = new MongoClient().getDB("qgrs");
-		Jongo jongo = new Jongo(db);
-		alignments = jongo.getCollection("alignments");
+		try {
+			DB db = new MongoClient().getDB("qgrs");
+			Jongo jongo = new Jongo(db);
+			alignments = jongo.getCollection("alignments");
+		}
+		catch (Exception e) {
+			System.err.println("Error connecting to MongoDB qgrs database.");
+			alignments = null;
+		}
 	}
 	
 	@Override
 	protected AlignmentProviderResult getAlignmentFromCache(GeneSequencePair pair) {
-		System.out.println("Finding alignment for " + pair.getPrinciple().getAccessionNumber() + " x " + pair.getComparison().getAccessionNumber());
-		Alignment alignment = alignments.findOne("{$and: [{principal.accessionNumber:#}, {comparison.accessionNumber:#}]}", 
-				pair.getPrinciple().getAccessionNumber(), pair.getComparison().getAccessionNumber()).as(Alignment.class);
-		if ( alignment == null ) return null;
-		return new AlignmentProviderResult(alignment.getPrincipal().getAlignedSequence(), alignment.getComparison().getAlignedSequence(), false);
+		if ( this.alignments == null ) return null;
+			System.out.println("Finding alignment for " + pair.getPrinciple().getAccessionNumber() + " x " + pair.getComparison().getAccessionNumber());
+			try {
+			Alignment alignment = alignments.findOne("{$and: [{principal.accessionNumber:#}, {comparison.accessionNumber:#}]}", 
+					pair.getPrinciple().getAccessionNumber(), pair.getComparison().getAccessionNumber()).as(Alignment.class);
+			if ( alignment == null ) return null;
+			return new AlignmentProviderResult(alignment.getPrincipal().getAlignedSequence(), alignment.getComparison().getAlignedSequence(), false);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	
