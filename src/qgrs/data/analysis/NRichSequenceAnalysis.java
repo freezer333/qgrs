7cd24d7547183bf8719fcdaaaf491c6b389a082c
freezer333 <scott.frees@gmail.com>
2014-04-02 15:52:13 -0400

    NRich Normalization

--- src/qgrs/data/analysis/NRichSequenceAnalysis.java
+++ src/qgrs/data/analysis/NRichSequenceAnalysis.java
@@ -18,10 +18,18 @@ public class NRichSequenceAnalysis {
 	private final MongoCollection nRichCollection;
 	private int mrnaCount = 0;
 	Jongo jongo;
+	int [] polyADataCount = new int [100];
+	public static int test =0;
+	
 	
 	public static void main(String[] args) throws UnknownHostException {
 		NRichSequenceAnalysis analysis = new NRichSequenceAnalysis();
 		analysis.run();
+		System.out.println ("**************************************");
+		System.out.println (" "+test);
+	    int [] pA = analysis.getPA();
+		for (int i = 0; i < 100; i ++)
+			System.out.println (i + "\t" + pA [i]);
 	}
 	
 	public NRichSequenceAnalysis() throws UnknownHostException {
@@ -38,6 +46,7 @@ public class NRichSequenceAnalysis {
 			try {
 				runMrna(mrna);
 				i++;
+				
 				//if ( i > 100 ) return;
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -46,12 +55,17 @@ public class NRichSequenceAnalysis {
 		}
 	}
 	
+	private int [] getPA ()
+	{
+		return polyADataCount;
+	}
 	
 	private void runMrna(MRNA mrna) throws Exception {
 		MongoSequenceProvider p = new MongoSequenceProvider(jongo);
 		mrnaCount++;
 		String sequence = p.getPrincipalByAccession(mrna.getAccessionNumber()); 
 		sequence = sequence.replaceAll("T", "U");
+		
 		
 		Range [] sites = new Range[mrna.getPolyASites().size()];
 		int c = 0;
@@ -67,6 +81,21 @@ public class NRichSequenceAnalysis {
 			int end = (i == sites.length-1) ? mrna.getSequenceLength()-1 : sites[i+1].getStart();
 			if ( end - start < 5) continue;
 			String slice = sequence.substring(start, end);
+			
+			int length = slice.length ();
+			
+			if (length > 500)
+				length = 500;
+			
+			int amt = length/5 -1;
+			
+			if (amt == -1)
+				polyADataCount[0]++;
+			
+			for (int k = 0; k < amt; k ++)
+				polyADataCount [k] ++;
+			
+			
 			for ( char n :ns ) {
 				Collection<NRich> us = new NRichFinder(n, slice).getAll(3);  
 				for ( NRich u : us ) {
@@ -79,6 +108,11 @@ public class NRichSequenceAnalysis {
 		}
 		
 		System.out.println("mrna [" + mrnaCount + "] -> sites [" + sites.length + "] ");
+		if (sites.length != 0)
+			test ++;
+			
+		
+	
+		
 	}
-
 }
